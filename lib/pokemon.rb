e0449fc0f3cb9fc06a5caaac4d6815168dccf0f7
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-05-02 15:20:35 -0700

    commit tree 40edb17d584c5ad697b7495ae2a708736705cf10

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(options={})
+        options.each do |key, value|
+        self.send("#{key}=", value) if respond_to?("#{key}=")
+        end
+    end
+
+    def self.save(name, type, db)
+        db.execute(
+          "INSERT INTO pokemon (name, type)
+          VALUES (?, ?)",name, type)
+    end
+
+    def self.find(idn, database)
+        found_pokemon = database.execute(
+            "SELECT * FROM pokemon 
+            WHERE id=?", idn).flatten
+        Pokemon.new(id: found_pokemon[0], name: found_pokemon[1], type: found_pokemon[2])
+    end
+
 end
